e689dc126a086a76888959459f588bf093f0b8d3
Brionna White <Brionnanwhite@gmail.com>
2016-12-15 11:27:13 -0800

    install request & express

---
+++ api/movie.ts
@@ -0,0 +1,35 @@
+// // import modules
+// import express = require ('express');
+// let router = express.Router();
+// let mongoose = require('mongoose');
+// let request = require('request');
+//
+// //MODEL
+// let Movie = mongoose.model('Movie', {
+//   Movie:{
+//     Genre:String,
+//   },
+// });
+//
+// //save Recipe
+// router.post('/recipe', function(req, res) {
+//   let newRecipe = new Recipe ({
+//       recipe:req.body.recipe
+//   })
+//   request('http://food2fork.com/api/search?key=75cf831690ffc9d6e14a46dd5b3c8c13&q='+req.body.recipe,
+//     function (error, response, body) {
+//       console.log(body)
+//       let type = JSON.parse(body)
+//       if (type.name === req.body.type) {
+//         res.send(type);
+//       } else {
+//         console.log(error)
+//         res.send({message:'recipe not found'})
+//       }
+//     })
+//   });
+//
+//
+//
+// // EXPORT ROUTER
+// export = router;
