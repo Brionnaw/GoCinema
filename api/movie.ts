9647acefdc87de78de9ad20f0edb4fb75f8c4ed8
Brionna White <Brionnanwhite@gmail.com>
2016-12-25 06:17:24 -0800

    add css

--- api/movie.ts
+++ api/movie.ts
@@ -11,7 +11,7 @@ let Movie = mongoose.model('Movie', {
     movie:String,
   },
 });
-//The Movie DB api
+//The IMDb api
 router.post('/movie', function(req, res) {
   let newMovie = new Movie ({
       movie:req.body.movie,
@@ -29,7 +29,9 @@ router.post('/movie', function(req, res) {
   })
 });
 
-// https://api.themoviedb.org/3/search/movie?api_key=27d9edaf269ac51be79e2c8fc19e5c99&callback=test&append_to_response=videos,images&query='+req.body.movie,
+
+
+// https://api.themoviedb.org/3/search/movie?api_key=27d9edaf269ac51be79e2c8fc19e5c99&callback=test&append_to_response=videos,images&include_image_language=en,null&query='+req.body.movie,
 // Gracenote api for local movie threatres:
 // http://data.tmsapi.com/v1.1/movies/showings?startDate='+req.body.date+'&numDays=2&zip='+req.body.zipCode+'&units=mi&imageSize=Md&imageText=false&api_key=fr63fvhma63cuws5jkzy22eh'
 
