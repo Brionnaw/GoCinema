2ff6b7af5b8f4c6b45b8ed462b86b636979e8fe4
Brionna White <Brionnanwhite@gmail.com>
2016-12-29 06:05:39 -0800

    add

--- api/showtimes.ts
+++ api/showtimes.ts
@@ -1,39 +1,39 @@
-// // import modules
-// import express = require ('express');
-// let router = express.Router();
-// let mongoose = require('mongoose');
-// let request = require('request');
-//
-//
-// //Showtime model
-// let Showtimes = mongoose.model('Showtimes', {
-//   Showtimes:{
-//     zipCode:String,
-//     date:String
-//   },
-// });
-//
-//
-// //The Gracenote api - movie showtimes
-// router.post('/showtimes', function(req, res) {
-//   let newShowtimes = new Showtimes ({
-//       zipCode:req.body.zipCode,
-//       date:req.body.date
-//   })
-//   request('http://data.tmsapi.com/v1.1/movies/showings?startDate='+req.body.date+'&numDays=2&zip='+req.body.zipCode+'&units=mi&imageSize=Md&imageText=false&api_key=fr63fvhma63cuws5jkzy22eh',
-//   function (error, response, body) {
-//     let data = JSON.parse(body) // parse data
-//         console.log(data)
-//         if (!error && response.statusCode == 200) {
-//         res.send(data) // send the parse data to front end
-//       } else {
-//         console.log(error)
-//       res.send(error)
-//     }
-//   })
-// });
-//
-//
-//
-// // export router
-// export = router;
+// import modules
+import express = require ('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+let request = require('request');
+
+
+//Showtime model
+let Showtimes = mongoose.model('Showtimes', {
+  Showtimes:{
+    zipCode:String,
+    date:String
+  },
+});
+
+
+//The Gracenote api - movie showtimes
+router.post('/showtimes', function(req, res) {
+  let newShowtimes = new Showtimes ({
+      zipCode:req.body.zipCode,
+      date:req.body.date
+  })
+  request('http://data.tmsapi.com/v1.1/movies/showings?startDate='+req.body.date+'&numDays=2&zip='+req.body.zipCode+'&units=mi&imageSize=Md&imageText=false&api_key=fr63fvhma63cuws5jkzy22eh',
+  function (error, response, body) {
+    let data = JSON.parse(body) // parse data
+        console.log(data)
+        if (!error && response.statusCode == 200) {
+        res.send(data) // send the parse data to front end
+      } else {
+        console.log(error)
+      res.send(error)
+    }
+  })
+});
+
+
+
+// export router
+export = router;
--- ngApp/js/services.ts
+++ ngApp/js/services.ts
@@ -12,21 +12,21 @@ namespace app.Services {
        this.MovieResource = $resource('api/movie');
    }
  }
- // export class ShowtimesService {
- //   public ShowtimeResource;
- //   public getTimes(getInfo){
- //     console.log(getInfo)
- //     return this.ShowtimeResource.save(getInfo).$promise
- //   }
- //   constructor(
- //     $resource:ng.resource.IResourceService,
- //   ){
- //       this.ShowtimeResource = $resource('api/showtimes');
- //   }
- // }
+ export class ShowtimesService {
+   public ShowtimeResource;
+   public getTimes(getInfo){
+     console.log(getInfo)
+     return this.ShowtimeResource.save(getInfo).$promise
+   }
+   constructor(
+     $resource:ng.resource.IResourceService,
+   ){
+       this.ShowtimeResource = $resource('api/showtimes');
+   }
+ }
 
- angular.module('app').service('movieService', MovieService)
- // angular.module('app').service('showtimesService', ShowtimesService)
+ angular.module('app').service('movieService', MovieService),
+ angular.module('app').service('showtimesService', ShowtimesService)
 
 
 }
